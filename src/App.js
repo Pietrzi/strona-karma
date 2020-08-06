34c7f6764def9ce3c542e1751d31c05d61e24927
Pietrzi <pietrzi@op.pl>
2020-08-06 18:22:31 +0200

    made component structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Theater from './components/Theater';
+import Film from './components/Film';
+import Gallery from './components/Gallery';
+import News from './components/News';
+import Bio from './components/Bio';
+import Contact from './components/Contact';
+
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
+      <Home />
+      <Theater />
+      <Film />
+      <Gallery />
+      <News />
+      <Bio />
+      <Contact />
     </div>
   );
 }
